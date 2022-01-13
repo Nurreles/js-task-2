3d508d50a307bb4828f936e8e48b5a4c8ddc15f1
Nureles Abdullaev <nureles.abdullaiuulu.ss@gmail.com>
2022-01-13 23:54:55 +0600

    add task1

---
+++ js/task1.js
@@ -0,0 +1,6 @@
+// Дана строка 'ddd@bbb@ccc'. Замените все @ на '!'
+
+let string = 'ddd@bbb@ccc';
+let newString = string.replace("@", "!");
+let strRes = newString.replace("@", "!");
+console.log(strRes);
